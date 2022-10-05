0ac7151b94ffca24fa1fa9d703dfd51502a07c5c
tristan-cyber <tristanwestreicher@gmail.com>
2022-10-05 20:46:19 +0200

    no message

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,21 +6,78 @@ public class App {
 
     //todo Task 1
     public void largestNumber(){
+
+        int counter = 1;
+        double largest;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number "+ counter++ + ": ");
+        double number = largest = scanner.nextDouble();
+
+        if (number <= 0) {
+            System.out.println("No number entered.");
+        } else {
+
+            while (number > 0) {
+                System.out.print("Number " + counter + ": ");
+                number = scanner.nextDouble();
+
+                if (largest < number) {
+                    largest = number;
+                }
+
+                counter++;
+
+            }
+
+            System.out.printf("The largest number is %.2f" + System.lineSeparator(), largest);
+        }
         // input your solution here
     }
 
     //todo Task 2
     public void stairs(){
+        Scanner scanner = new Scanner(System.in);
+
+        final int ROWS = scanner.nextInt();
+        System.out.print("n: ");
+        int runningvariable = 1;
+
+        if (ROWS >= 0) {
+            for (int row = 0; row < ROWS; row++) {
+                for (int col = ROWS; col >= ROWS - row; col--) {
+                    System.out.print(runningvariable + " ");
+                    runningvariable++;
+                }
+                System.out.println();
+            }
+        } else System.out.println("Invalid number!");
         // input your solution here
     }
 
     //todo Task 3
     public void printPyramid(){
-        // input your solution here
+        int ROWS = 6;
+
+        for (int row = 1; row <= ROWS; row++){
+            for (int col = ROWS; col > row; col--) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < row*2-1; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+
+
+    // input your solution here
     }
 
     //todo Task 4
     public void printRhombus(){
+        // zum char einlesen char c = scan.next().charAt(0);
         // input your solution here
     }
 
